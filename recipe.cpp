4d021e762defa50b3319957c4961536a04fcdd77
PancakeSyrup24 <benedictoargel@gmail.com>
2026-05-13 15:10:53 +0800

    Recipe Updated

--- recipe.cpp
+++ recipe.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 int main() {
     std::cout << "Chocolate Chip Cookies - 4 dozen cookies" << std::endl;
+
+    std::cout << "\n\t 1 cup butter\n"
+    << "\t 1 1/2 cups white sugar \n"
+    << "\t 2 eggs \n"
+    << "\t 2 tsp vanilla extract\n"
+    << "\t 2 cups all-purpose flour\n"
+    << "\t 3/4 baking soda \n"
+    << "\t 1/4 tsp salt \n"
+    << "\t 2 cups chocolate chips" << std::endl;
+
+    std::cout << "\n Preheat oven to 350 F. In a large bowl, mix butter, sugar, eggs, and vanilla \n"
+    << " until light and fluffy. In a separate bowl, combine flour, baking soda and \n"
+    << " salt; stir into the butter mixture until well-blended stir in chocolate chios . \n"
+    << " Drop by rounded teaspoons onto ungreased cookie sheets. Bake 8-10 minutes until \n"
+    << " just set. Cool slightly on cookie sheets before transferring to cooling racks \n"
+    << " to cool completely." << std::endl;
+
     return 0;
-}
